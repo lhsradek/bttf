43a20d0120ef531f349e9487ad211d4215cdefd3
Radek Kadner <radek.kadner@gmail.com>
2023-02-15 23:20:35 +0100

    .

--- bttf-impl/src/main/kotlin/local/intranet/bttf/api/domain/Countable.kt
+++ bttf-impl/src/main/kotlin/local/intranet/bttf/api/domain/Countable.kt
@@ -7,24 +7,24 @@ import com.fasterxml.jackson.annotation.JsonProperty
  *
  * {@link Countable} for {@link local.intranet.core.api.info.JobInfo},
  * {@link local.intranet.core.api.service.JobService}
- * 
- * 
+ *
+ *
  * @author Radek Kádner
  *
  */
 public interface Countable {
 
-	/**
-	 * 
-	 * Number of invocations
-	 * <p>
-	 * &#64;JsonProperty("count")
-	 * 
-	 * @return number of invocations from count
-	 */
-	@JsonProperty("count")
-	public fun countValue(): Long
-    
+    /**
+     *
+     * Number of invocations
+     * <p>
+     * &#64;JsonProperty("count")
+     *
+     * @return number of invocations from count
+     */
+    @JsonProperty("count")
+    public fun countValue(): Long
+
     /**
      *
      * Increment counter
--- bttf-impl/src/main/kotlin/local/intranet/bttf/api/domain/LoginCache.kt
+++ bttf-impl/src/main/kotlin/local/intranet/bttf/api/domain/LoginCache.kt
@@ -11,66 +11,66 @@ import org.jetbrains.annotations.NotNull
 /**
  *
  * https://hackmd.io/@pierodibello/ByEVNHg-v
- */ 
+ */
 public interface LoginCache {
 
     /**
      *
      * It doesn't have to exist, if exist return number of attempts
-     * 
+     *
      * @param key {@link String}
      * @return {@link Int?}
      */
     @Size(min = 1, max = DefaultFieldLengths.DEFAULT_NAME)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public fun getById(@NotNull key: String): Int?
-    
+
     /**
      *
      * Put new key or increment number of attempts
      *
      * @param key {@link String}
-     */ 
+     */
     @Size(min = 1, max = DefaultFieldLengths.DEFAULT_NAME)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public fun keyToCache(@NotNull key: String)
-    
+
     /**
      *
      * login succeeded, remove key from cache
      *
      * @param key {@link String}
-     */           
+     */
     @Size(min = 1, max = DefaultFieldLengths.DEFAULT_NAME)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public fun invalidateKey(@NotNull key: String)
-    
+
     /**
      *
      * Is blocked? Key doesn't have to exist
      *
      * @param key {@link String}
      * @return {@link Boolean?}
-     */ 
+     */
     @Size(min = 1, max = DefaultFieldLengths.DEFAULT_NAME)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public fun isBlocked(@NotNull key: String): Boolean
-    
+
     /**
      *
      * key, number of attempts, creation timestamp of all
-     * 
+     *
      * @param printBlocked {@link Boolean?} as filter if not null
      * @return {@link List}&lt;{@link AttemptInfo}&gt;
      */
     @Size(min = 1, max = DefaultFieldLengths.DEFAULT_NAME)
     public fun getCache(printBlocked: Boolean?): List<AttemptInfo>
-    
+
     /**
      *
      * Remove Expired Keys
      */
     @JsonIgnore
     public fun removeExpiredKeys()
-    
+
 }
